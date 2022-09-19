b28fc0eeadc5d61a3d3173b51bc2171a01bb88c2
Nathalia Marana <nathalia_marana@hotmail.com>
2022-09-19 18:42:15 -0300

    Exercicio 2

--- Fundamentos/Bloco7-ES6-Testes-Unitários/Dia2-Fluxo-de-excecao-e-Objetos/script.js
+++ Fundamentos/Bloco7-ES6-Testes-Unitários/Dia2-Fluxo-de-excecao-e-Objetos/script.js
@@ -1,13 +1,63 @@
-const band = {
-    name: 'Blind Guardian',
-    formedIn: 1986,
-    lastAlbum: 'Beyond the Red Mirror',
-  };
-  
-  const info = {
-    genre: 'Power Metal',
-    lastAlbum: 'Twilight Orchestra: Legacy of the Dark Lands',
-  };
-  
-  
-  console.log(Object.assign(band, info));
+const order = {
+  name: "Rafael Andrade",
+  phoneNumber: "11-98763-1416",
+  address: {
+    street: "Rua das Flores",
+    number: "389",
+    apartment: "701",
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: "Coca-Cola Zero",
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: "Ana Silveira",
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  const address = "address";
+  const deliveryPerson = order.order.delivery.deliveryPerson;
+  const customerName = order.name;
+  const customerPhone = order.phoneNumber;
+  const street = order[address].street;
+  const number = order[address].number;
+  const apartment = order[address].apartment;
+
+  console.log(
+    `Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone: ${customerPhone}, R. ${street}, Nº: ${number}, AP: ${apartment}`
+  ); //
+};
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  const name = order.name = 'Luiz Silva';
+  const total = order.payment.total = '50';
+  const pizza1 = Object.keys(order.order.pizza);
+  const pizza2 = Object.keys(order.order.pizza);
+  const bebida = order.order.drinks.coke.type;
+ 
+  console.log(`Olá ${name}, o total do seu pedido de ${pizza1}, ${pizza2} e ${bebida} é de R$${total},00`)
+};
+
+orderModifier(order);
