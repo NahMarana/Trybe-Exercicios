0c1824bb69eb5856e0f63681d71c292c1ec5182b
Nathalia Marana <nathalia_marana@hotmail.com>
2022-09-23 19:43:17 -0300

    Exercícios do dia

--- Fundamentos/Bloco 8 - Higher Order Functions ES6/Dia2-forEach-find-some-every-sort/find2.js
+++ Fundamentos/Bloco 8 - Higher Order Functions ES6/Dia2-forEach-find-some-every-sort/find2.js
@@ -1,4 +1,4 @@
-const names = ['João', 'Irene', 'Fernando', 'Maria'];
+const names = ["João", "Irene", "Fernando", "Maria"];
 
 const findNameWithFiveLetters = () => {
   return names.find((nome) => nome.length === 5);
--- Fundamentos/Bloco 8 - Higher Order Functions ES6/Dia2-forEach-find-some-every-sort/some.js
+++ Fundamentos/Bloco 8 - Higher Order Functions ES6/Dia2-forEach-find-some-every-sort/some.js
@@ -1,7 +1,7 @@
-const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+const names = ["Mateus", "José", "Ana", "Cláudia", "Bruna"];
 
 const hasName = (arr, name) => {
-  return arr.some((nome) => nome === name)
+  return arr.some((nome) => nome === name);
 };
 
-console.log(hasName(names, 'Ana'));
+console.log(hasName(names, "Ana"));
