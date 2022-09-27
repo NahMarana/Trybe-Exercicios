b1dfcdb3c4647f81c1955c38b7aca77c1127fe06
Nathalia Marana <nathalia_marana@hotmail.com>
2022-09-27 19:46:42 -0300

    Exercicios do dia

---
+++ Fundamentos/Bloco 8 - Higher Order Functions ES6/Dia4-Reduce/script5.js
@@ -0,0 +1,17 @@
+const names = [
+  "Aanemarie",
+  "Adervandes",
+  "Akifusa",
+  "Abegildo",
+  "Adicellia",
+  "Aladonata",
+  "Abeladerco",
+  "Adieidy",
+  "Alarucha",
+];
+
+function containsA() {
+
+}
+
+console.log(containsA());
